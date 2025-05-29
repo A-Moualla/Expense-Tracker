ecf52c3cd07937b404fabd484ad1cf6b889efd72
A-Moualla <ammar13a2r@gmail.com>
2025-05-29 16:21:32 +0300

    add useState to control the component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const Form = () => {
+  const [anotherPerson, setAnotherPerson] = useState({ name: "aa", age: "12" });
   const nameRef = useRef(null);
   const ageRef = useRef(null);
   const person = {
@@ -12,6 +13,7 @@ const Form = () => {
     person.name = nameRef.current.value;
     person.age = ageRef.current.value;
     console.log(person);
+    console.log(anotherPerson);
   };
 
   return (
@@ -20,13 +22,31 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} id="name" type="text" className="form-control" />
+        <input
+          ref={nameRef}
+          onChange={(e) =>
+            setAnotherPerson({ ...anotherPerson, name: e.target.value })
+          }
+          value={anotherPerson.name}
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input
+          ref={ageRef}
+          onChange={(e) =>
+            setAnotherPerson({ ...anotherPerson, age: e.target.value })
+          }
+          value={anotherPerson.age}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
